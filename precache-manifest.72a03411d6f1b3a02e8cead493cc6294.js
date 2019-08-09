8d13a54b12d05755e18965f7456bb47645ca247e
sly92 <codingsly@gmail.com>
2019-08-09 15:57:09 +0200

    Updates

---
+++ precache-manifest.72a03411d6f1b3a02e8cead493cc6294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6519da46982249a25429c8e77b69199d",
+    "url": "/chatBox/index.html"
+  },
+  {
+    "revision": "e6eed7c298b73b45f005",
+    "url": "/chatBox/static/css/main.dda847ca.chunk.css"
+  },
+  {
+    "revision": "2160fab68be80c74e636",
+    "url": "/chatBox/static/js/2.bc5905f4.chunk.js"
+  },
+  {
+    "revision": "e6eed7c298b73b45f005",
+    "url": "/chatBox/static/js/main.239d2838.chunk.js"
+  },
+  {
+    "revision": "a30829fc167cc0afea09",
+    "url": "/chatBox/static/js/runtime~main.0d18df34.js"
+  }
+]);
