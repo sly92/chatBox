83635899c4adf19165f0ee5ab8934741feee114b
sly92 <codingsly@gmail.com>
2019-08-09 16:05:07 +0200

    Updates

--- precache-manifest.0fd8250939f938dee95f6d2a43aac2e3.js
+++ precache-manifest.0fd8250939f938dee95f6d2a43aac2e3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1bab18d40283593305c518944663e60e",
+    "revision": "2b77e5d33a055ec0d979e604f960b05e",
     "url": "/chatBox/index.html"
   },
   {
-    "revision": "1716f1702e29563932ec",
+    "revision": "726d640a8db7c0ebcabf",
     "url": "/chatBox/static/css/main.dda847ca.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chatBox/static/js/2.bc5905f4.chunk.js"
   },
   {
-    "revision": "1716f1702e29563932ec",
-    "url": "/chatBox/static/js/main.0d1a74fc.chunk.js"
+    "revision": "726d640a8db7c0ebcabf",
+    "url": "/chatBox/static/js/main.bc2beafa.chunk.js"
   },
   {
     "revision": "a30829fc167cc0afea09",
